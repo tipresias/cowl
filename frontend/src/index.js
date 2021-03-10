a0489f44ff9c283bdc82b62723995a56846b074f
Craig Franklin <craigjfranklin@gmail.com>
2021-03-11 09:54:27 +1100

    Actually fix ApolloClient uri
    I'm too accustomed to backend coding, and the idea of code
    running in the browser environment keeps tricking me into
    making foolish mistakes. Obviously, we want to call the API
    of the production host, not the localhost of whoever opens
    the site.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const domainPort = process.env.NODE_ENV === 'production' ? 'tipresias.net' : 'localhost:8000';
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  uri: `http://${domainPort}/graphql`,
   cache: new InMemoryCache({
     addTypename: false,
   }),
