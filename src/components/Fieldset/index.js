40afe4725cc9a0dabd9f1b72a677d373d51d03df
Craig Franklin <craigjfranklin@gmail.com>
2021-05-17 15:39:33 +1000

    Auto-fix import order

--- src/components/Fieldset/index.js
+++ src/components/Fieldset/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
-import type { Node } from 'react';
 import styled from 'styled-components/macro';
+import type { Node } from 'react';
 
 const FieldsetStyled = styled.fieldset`
 display: flex;
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
-import type { Node } from 'react';
 import styled from 'styled-components/macro';
+import type { Node } from 'react';
 
 const StyledTable = styled.table`
   width: 100%;
