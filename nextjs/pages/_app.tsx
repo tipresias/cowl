1f56b3a62f9a293f35191f6f064a3505a496299d
Craig Franklin <craigjfranklin@gmail.com>
2022-01-06 09:55:25 +1100

    Add Apollo client to app
    Left the cache type as 'any', because it will be easier to define
    it once we fetch the GQL types from the API.

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const defineHost = () => {
+  if (process.env.NODE_ENV === "production") return "206.189.38.8";
+
+  return "localhost:8000";
+};
+
+const host = defineHost();
+const client = new ApolloClient({
+  uri: `http://${host}/graphql`,
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default App;
