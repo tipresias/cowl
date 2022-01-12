ab887ce29ac13a1264f9a57089018b443db81b75
Craig Franklin <craigjfranklin@gmail.com>
2022-01-12 17:10:34 +1100

    Use env var to define the API host
    This is a bit more flexible than basing it on the environment
    and hard-coding the IP/domain.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import Layout from "../components/Layout";
 
-const defineHost = () => {
-  if (process.env.NODE_ENV === "production") return "206.189.38.8";
-
-  return "localhost:8000";
-};
-
-const host = defineHost();
+const host = process.env.API_HOST || "localhost:8000";
 const client = new ApolloClient({
   uri: `http://${host}/graphql`,
   cache: new InMemoryCache({
